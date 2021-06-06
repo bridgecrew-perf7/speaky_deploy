b535711c6a2c59554e54ab5e87e033c03f7f4b1c
Muhammad Luthfi <mluthfi3d@gmail.com>
2021-06-06 12:50:13 +0700

    change the parsing url

--- app.py
+++ app.py
@@ -110,7 +110,8 @@ def api():
     uid = request.args.get('uid')
 
     url = filepath + "&token=" + token
-    url = url.replace('video/', 'video%2F').replace('/video-', '%2Fvideo-')
+    url = url.replace('%3A', ':').replace('%2F', '/').replace('%3D', '=').replace('%3F', '?').replace('%26', '&')
+    url = url.replace('video%252F', 'video%2F').replace('%252Fvideo-', '%2Fvideo-')
 
     global parent_path
     parent_path = "data/" + uid
